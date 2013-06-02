7bed853b2ee6b6e30caa88dddd063c412316c883
hiromitsu <hitsk26@gmail.com>
2013-06-02 15:49:39 +0900

    implemented calcCurvature method and R getter

--- SHITAKE/templete/CurvatureDetecter.c
+++ SHITAKE/templete/CurvatureDetecter.c
@@ -1,7 +1,31 @@
 #include "CurvatureDetecter.h"
 
 
-float detectCurvature(){
-	return 0;
+void calcCurvature(){
+	
+	float fi_l = (float)nxt_motor_get_count(NXT_PORT_C);
+	float fi_r = (float)nxt_motor_get_count(NXT_PORT_B);
+	float dist_l = deg2rad(fi_l * W_RADIUS);
+	float dist_r = deg2rad(fi_r * W_RADIUS);
+	static float dist_t = 0,theta_t=0;
+	static float dist = 0;		//ˆÚ“®‹——£
+
+
+	dist = (dist_l + dist_r) / 2.0;
+	theta = W_RADIUS / W_DIST * (fi_r - fi_l);
+	x_r += (dist - dist_t) * sin(deg2rad(theta));
+	y_r += (dist - dist_t) * cos(deg2rad(theta));
+	if(!(theta == theta_t)){
+		R = rad2deg((dist - dist_t) / (theta - theta_t));
+	}
+	else{
+		R= 0.0;
+	}
+	dist_t = dist;
+	theta_t = theta;
+}
+
+float getCurvature(){
+	return R;
 }
 
--- SHITAKE/templete/CurvatureDetecter.h
+++ SHITAKE/templete/CurvatureDetecter.h
@@ -1,8 +1,28 @@
 #ifndef _CurvatureDetector_
 #define _CurvatureDetector_
 
+#include "mymath.h"
+#include "math.h"
+#include "ecrobot_interface.h"
 
-float detectCurvature();
+// 車輪半径、走行体幅
+#define W_RADIUS	4.1	//[mm]
+#define W_DIST		16.2//[mm]
+//S___________________________ */
+
+#define CIRCUMFERENCE	25.8	//車輪の円周
+
+/* 自己位置同定用変数 */
+static float x_r = 0;		//車体のX座標
+static float y_r = 0;		//車体のY座標
+
+static float theta = 0;		//車体の角度
+static float R = 0;			//曲率半径
+
+
+
+void calcCurvature();
+float getCurvature();
 
 
 #endif
